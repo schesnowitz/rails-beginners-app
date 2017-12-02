fb369403c0ea2f8b8ba0a6be5af3f0af2cbc8950
Steve Chesnowitz <steve@chesnowitz.com>
2017-12-02 14:20:15 -0500

    template ones section

--- app/assets/javascripts/template_ones/manifest.js
+++ app/assets/javascripts/template_ones/manifest.js
@@ -3,3 +3,15 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+
+ 
+//= require template_ones/jquery.easing/jquery.easing
+//= require template_ones/owl-carousel/owl.carousel
+//= require template_ones/magnific-popup/jquery.magnific-popup
+//= require template_ones/vide/jquery.vide
+//= require template_ones/mixitup/mixitup
+//= require template_ones/wowjs/wow
+
+//= require template_ones/js/contact_me
+//= require template_ones/js/jqBootstrapValidation
