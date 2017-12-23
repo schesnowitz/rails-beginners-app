446d84fa1831a41d5d5131abd25e2277bbd778d6
Steve Chesnowitz <steve@chesnowitz.com>
2017-12-23 07:44:39 -0500

    jquery and final deploy

---
+++ app/assets/javascripts/template_ones/header_module.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#template_one_header_title').keyup(function() {
+   $('#header_title_show').html($(this).val()); 
+ });
+
+ $('#template_one_header_text').keyup(function() {
+  $('#header_text_show').html($(this).val()); 
+ });
+});
--- app/assets/javascripts/template_ones/manifest.js
+++ app/assets/javascripts/template_ones/manifest.js
@@ -4,8 +4,17 @@
 //= require popper
 //= require bootstrap-sprockets
 
+//= require template_ones/header_module.js
+//= require template_ones/about_module.js 
+//= require template_ones/service_module.js
+//= require template_ones/action_module.js 
+//= require template_ones/process_module.js  
+//= require template_ones/carousel_heading_module.js 
+//= require template_ones/portfolio_grid_module.js  
+//= require template_ones/contact_module.js  
+//= require template_ones/footer_module.js
 
- 
+
 //= require template_ones/jquery.easing/jquery.easing.js
 //= require template_ones/owl-carousel/owl.carousel.js
 //= require template_ones/magnific-popup/jquery.magnific-popup.js
