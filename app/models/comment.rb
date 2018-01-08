66367108720c13df943429616fcbee8dac2c7cf8
Steve Chesnowitz <steve@chesnowitz.com>
2018-01-08 11:20:13 -0500

    comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, length: { minimum: 4, maximum: 300 } 
+  # validates :body, length: { minimum: 4, maximum: 300 } 
 end
